9122f32f523bc264571f2c3fe8c715fdef3a8205
Rosana Moreira <rooh.moreiraa@gmail.com>
2021-09-09 16:38:49 -0300

    parte html pronta

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# jogo-da-forca
+# jogo-da-forca# 
+
+# Descrição do Projeto
+<p align="center">
+Jogo desenvolvido para curso da IBM Winning Woman - Primeira Fase.
+</p>
+
+<h1 align="center">
+  <img alt="logo" title="#logo" src="./assets/git.gif" />
+</h1>
+
+### 🎲 Executando o codigo fonte
+
+```bash
+# Clone este repositório
+$ git clone <https://github.com/rosana-moreira/jogo-da-forca.git/>
+
+# Acesse a pasta do projeto no terminal/cmd
+$ cd jogo-da-forca
+
+# Abra o arquivo index.html pelo o navegador
+$ index.html
+
+```
---
+++ script.js
@@ -0,0 +1,72 @@
+/* Elemento HTML referente a categoria */
+const categoria = document.querySelector("#category");
+/* Elemento HTML referente a lista das letras erradas*/
+const letrasErradas = document.querySelector(".wrongLetters");
+/* Elemento HTML referente a palavra oculta
+   Utilizaremos esse mesmo elemento para exibir as mensagens do jogo*/
+   const palavraInterface = document.querySelector(".dashes");
+/* Array com elementos HTML referentes aos olhos do personagem */
+const olhos = Array.from(document.querySelectorAll(".eyes"));
+/* Array com elementos HTML referentes as partes do corpo */
+let partesBoneco = Array.from(document.querySelectorAll("#person div"));
+partesBoneco = partesBoneco.slice(2, partesBoneco.length);
+/* Palavra corrente */
+let palavraProposta;
+/* Lista das letras erradas */
+let letrasErradasArray = [];
+/* Index da parte do corpo corrente */
+let indiceBoneco;
+/* Numero de chances do jogador */
+const numTentativas = 7;
+/* Valor para opacidade dos olhos */
+const opacidadeOlhos = 0.3;
+
+
+/*
+Recebe o evento do teclado e passa apenas o valor da letra para a função tentativa
+*/
+function retornaLetra(e){ 
+    tentativa(e.key);
+}
+
+/*
+Desenha a parte do corpo corrente
+*/
+function desenhaBoneco(){
+    partesBoneco[indiceBoneco].classList.remove("hide");
+    indiceBoneco++; 
+}
+
+/* 
+Desenha os olhos do personagem
+*/
+function desenhaOlhos(){
+    olhos.forEach((olho => {
+        olho.style.opacity = 1;
+        olho.style.zIndex = 10;
+    }));
+}
+
+/*
+Oculta as partes do corpo do personagem
+*/
+function ocultaBoneco(){
+    olhos.forEach((olho => {
+        olho.style.opacity = opacidadeOlhos; 
+    }));
+    partesBoneco.forEach(parteBoneco => {
+        parteBoneco.classList.add("hide");
+    });
+}
+
+/*
+Inicia as configurações do jogo
+*/
+function iniciaJogo(){
+    indiceBoneco = 0;
+    letrasErradasArray = [];
+    letrasErradas.innerHTML = "Letras erradas: ";
+    window.addEventListener("keypress", retornaLetra);
+}
+
+window.addEventListener("load", iniciaJogo);
