0c0bad119c0601bbe7bf01d2813a787a8a3f0c5d
Rosana Moreira <rooh.moreiraa@gmail.com>
2021-09-13 10:02:39 -0300

    Funçao de esconder a palavra pronta

--- script.js
+++ script.js
@@ -21,6 +21,45 @@ const numTentativas = 7;
 /* Valor para opacidade dos olhos */
 const opacidadeOlhos = 0.3;
 
+const categorias = {
+    Frutas:["Banana","Maçã", "Laranja","Uva", "Melancia", "Melão"],
+    Profissoes:["Engenheiro","Desenvolvedor","Advogado", "Agronomo"],
+    Animais : ["Gato", "Galinha", "cavalo","Cachorro", "Raposa"],
+    Cores: ["amarelo","azul", "Laranja", "Roxo", "Rosa", "Vermelho"]
+};
+
+function retornaArrayCategorias(){
+    return Object.keys(categorias);
+};
+function retornaCategoria(){
+    const arrayCategorias = retornaArrayCategorias();
+    let indiceCategorias =retornaNumAleatorio(arrayCategorias.length);
+    return arrayCategorias[indiceCategorias];
+};
+function exibeCategoria(){
+    categoria.innerHTML = retornaCategoria();
+};
+function retornaNumAleatorio(max){
+    return Math.floor(Math.random() * max);
+};
+function definePalavraProposta(){
+    const arrayPalavras = categorias[categoria.innerHTML];
+    let indicePalavra = retornaNumAleatorio(arrayPalavras.length);
+    palavraProposta = arrayPalavras[indicePalavra];
+    ocultaPalavra();
+};
+function ocultaPalavra(){
+    let palavraOcultada = "";
+    for(let i = 0; i < palavraProposta.length;  i++ ){
+        palavraOcultada+="-";
+    }
+    exibePalavraInterface(palavraOcultada);
+    
+};
+function exibePalavraInterface(palavra){
+    palavraInterface.innerHTML = palavra;
+   
+}
 
 /*
 Recebe o evento do teclado e passa apenas o valor da letra para a função tentativa
@@ -65,6 +104,8 @@ Inicia as configurações do jogo
 function iniciaJogo(){
     indiceBoneco = 0;
     letrasErradasArray = [];
+    exibeCategoria();
+    definePalavraProposta();
     letrasErradas.innerHTML = "Letras erradas: ";
     window.addEventListener("keypress", retornaLetra);
 }
